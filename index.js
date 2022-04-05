b18cf2957fa7d87be14d6a1dc6775e410cce3384
Germán <56662567+grmnmtz@users.noreply.github.com>
2022-04-05 00:06:33 -0500

    add more actions to the reducer function and refactor from if else to switch statements

--- index.js
+++ index.js
@@ -67,11 +67,20 @@ Responsible for getting us to the next state of our application based on the spe
 */
 
 function reducer(state = [], action) {
-  if (action.type === 'ADD_TODO') {
-    return state.concat([action.todo])
+  switch (action.type) {
+    case 'ADD_TODO':
+      return state.concat([action.todo])
+    case 'REMOVE_TODO':
+      return state.filter((todo) => todo.id !== action.id)
+    case 'TOGGLE_TODO':
+      return state.map((todo) =>
+        todo.id !== action.id
+          ? todo
+          : Object.assign({}, todo, { complete: !todo.complete })
+      )
+    default:
+      return state
   }
-
-  return state
 }
 
 // This is our main function where we will be creating a new store.
